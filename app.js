77e7436ab6d0ca7cd1a9444ac3c01e7c6ccf1d10
gustavo <lgmamaniap@gmail.com>
2025-02-01 19:50:02 -0400

    feat: :construction: access and modify html element from JS

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-console.log(document);
-document.getElementById('main-h1').textContent = 'JS content'
-console.log(document.querySelector('h1'))
+/*
+document.querySelector('h3')
+console.log(document.querySelector('h3'))
+console.log(document.querySelector('.h3-danger'))
+console.log(document.querySelector('#paragraph'))
+console.log(document.getElementById('paragraph'))
+
+console.log(document.querySelectorAll('.h3-danger'))
+*/
+
+const paragraph = document.querySelector('#paragraph')
+paragraph.textContent = 'Text modified from JS'
+paragraph.innerHTML = '<b>Text with innerHTML</b>'
+
+paragraph.classList.add('h3-danger', 'my-2')
+
+const paragraph2 = document.getElementById('paragraph')
+paragraph2.textContent = 'Paragraph modified from JS'
+paragraph.innerHTML = '<b>Paragraph modified from JS using innerHTML</b>'
+
+paragraph.classList.add('h3-danger', 'mx-2')
