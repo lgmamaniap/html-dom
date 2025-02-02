231d42a8f5ec99a801a0f41f7b927ee3c17e9e95
gustavo <lgmamaniap@gmail.com>
2025-02-01 20:08:53 -0400

    feat: :construction: createElement function

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ console.log(document.querySelector('#paragraph'))
 console.log(document.getElementById('paragraph'))
 
 console.log(document.querySelectorAll('.h3-danger'))
-*/
+
 
 const paragraph = document.querySelector('#paragraph')
 paragraph.textContent = 'Text modified from JS'
@@ -19,3 +19,27 @@ paragraph2.textContent = 'Paragraph modified from JS'
 paragraph.innerHTML = '<b>Paragraph modified from JS using innerHTML</b>'
 
 paragraph.classList.add('h3-danger', 'mx-2')
+*/
+
+
+// Create element and innerHTML generates Reflow :()
+const list = document.getElementById('list')
+console.log(list)
+
+const arrayElements = [
+  'First element',
+  'Second element',
+  'Third element',
+]
+
+arrayElements.forEach((textLi, index) => {
+  const li = document.createElement('li')
+  li.id = `id-li-${index}`
+  li.textContent = textLi
+
+  list.appendChild(li)
+})
+
+arrayElements.forEach((textLi, index) => {
+  list.innerHTML += `<li id="id-li-${index}">${textLi}</li>`
+})
